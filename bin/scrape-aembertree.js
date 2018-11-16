03657f416092be8b36eaeb13f4b14d782048594f
Mitchel Pigsley <mpigsley@gmail.com>
2018-11-16 14:52:37 -0600

    Add power and armor to card data and simplify deck submission

--- bin/scrape-aembertree.js
+++ bin/scrape-aembertree.js
@@ -12,7 +12,7 @@ const scrape = async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto('http://aembertree.com/sets/core', {
-    waitUntil: 'networkidle2',
+    waitUntil: 'networkidle0',
   });
 
   const cardLinks = await page.$$('.card--link');
@@ -39,6 +39,8 @@ const scrape = async () => {
               getText(cardPage, '.cardview--house'),
               getText(cardPage, '.cardview--type'),
               getText(cardPage, '.cardview--traits'),
+              getText(cardPage, '.cardview--power'),
+              getText(cardPage, '.cardview--armor'),
               getText(cardPage, '.cardview--aember'),
               getText(cardPage, '.cardview--keyword'),
               getText(cardPage, '.cardview--text'),
@@ -46,7 +48,7 @@ const scrape = async () => {
               getText(cardPage, '.cardview--artist'),
             ]);
 
-            const aember = content[4].replace('Aember: ', '');
+            const aember = content[6].replace('Aember: ', '');
             const id = url[url.length - 1];
             const set = url[url.length - 2];
 
@@ -59,11 +61,13 @@ const scrape = async () => {
               house: content[1],
               type: content[2],
               traits: content[3],
+              power: content[4],
+              armor: content[5],
               aember: aember ? parseInt(aember, 10) : 0,
-              keyword: content[5],
-              text: content[6],
-              rarity: content[7],
-              artist: content[8].replace('Artist: ', ''),
+              keyword: content[7],
+              text: content[8],
+              rarity: content[9],
+              artist: content[10].replace('Artist: ', ''),
             };
           }),
         ).then(chunkData => [...cards, ...chunkData]),
--- functions/src/submit-deck.js
+++ functions/src/submit-deck.js
@@ -1,46 +1,31 @@
 const functions = require('firebase-functions');
-const puppeteer = require('puppeteer');
 
-const getText = (page, className) =>
-  page.$eval(className, element => element.innerText).catch(() => '');
+module.exports = functions.https.onCall(({ link }) => {
+  const splitLink = link.split('/');
+  const id = splitLink[splitLink.length - 1];
+  return request(`https://www.keyforgegame.com/api/decks/${id}?links=cards`)
+    .then(response => {
+      const { data, _linked } = JSON.parse(response);
+      const { name } = data;
+      const { cards } = _linked;
 
-module.exports = functions
-  .runWith({ memory: '1GB' })
-  .https.onCall(async ({ link }) => {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      const houses = cards.reduce(
+        (housesObj, { card_number, house }) => ({
+          ...housesObj,
+          [house.toLowerCase()]: [
+            ...(housesObj[house.toLowerCase()] || []),
+            card_number,
+          ],
+        }),
+        {},
+      );
+
+      return { name, houses };
+    })
+    .catch(() => {
+      throw new functions.https.HttpsError(
+        'not-found',
+        'Deck link is not valid',
+      );
     });
-    const page = await browser.newPage();
-    await page.goto(link, { waitUntil: 'networkidle0' });
-
-    const [deckName, houseLists] = await Promise.all([
-      getText(page, '.deck-page__deck-name'),
-      page.$$('.card-table__deck'),
-    ]);
-
-    const houses = {};
-    await Promise.all(
-      houseLists.map(async houseList => {
-        const [houseName, cardNumbers] = await Promise.all([
-          getText(houseList, '.card-table__deck-house-name'),
-          houseList.$$('.card-table__deck-card-number'),
-        ]);
-
-        const numberProperties = await Promise.all(
-          cardNumbers.map(number => number.getProperty('innerText')),
-        );
-        const numbers = await Promise.all(
-          numberProperties.map(number => number.jsonValue()),
-        );
-        houses[houseName.toLowerCase()] = numbers.map(num =>
-          num.substring(0, 3),
-        );
-      }),
-    );
-
-    await page.close();
-    await browser.close();
-
-    return { name: deckName, houses };
-  });
+});
