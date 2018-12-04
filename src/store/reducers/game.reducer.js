e0e2c10e643fe8414fa202054886fb2e46674c95
Mitchel Pigsley <mpigsley@gmail.com>
2018-12-04 14:03:22 -0600

    Update game reducer when one is fetched

--- src/store/reducers/game.reducer.js
+++ src/store/reducers/game.reducer.js
@@ -1,11 +1,17 @@
-import { LOCATION_CHANGE } from 'connected-react-router';
 import { SIGNED_OUT } from 'store/actions/user.actions';
+import { GAMES_UPDATED } from 'store/actions/game.actions';
 
-const initialState = {};
+import { omit } from 'constants/lodash';
+
+const initialState = { models: {} };
 
 export default function game(state = initialState, action) {
   switch (action.type) {
-    case LOCATION_CHANGE:
+    case GAMES_UPDATED.SUCCESS:
+      return {
+        ...state,
+        models: omit({ ...state.models, ...action.update }, ...action.deleted),
+      };
     case SIGNED_OUT.SUCCESS:
       return initialState;
     default:
