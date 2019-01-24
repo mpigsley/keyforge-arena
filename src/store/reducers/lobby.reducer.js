2bf6dae126b032d608630fcc8cf50bf422979638
Mitchel Pigsley <mpigsley@gmail.com>
2019-01-24 12:44:44 -0600

    Fix firebase/react upgrades

--- src/store/reducers/lobby.reducer.js
+++ src/store/reducers/lobby.reducer.js
@@ -1,5 +1,4 @@
 import {
-  // CHALLENGE,
   CANCEL_CHALLENGE,
   ACCEPT_CHALLENGE,
   LOBBIES_UPDATED,
@@ -15,7 +14,6 @@ const initialState = {
 
 export default function lobby(state = initialState, action) {
   switch (action.type) {
-    // case CHALLENGE.SUCCESS:
     case LOBBIES_UPDATED.SUCCESS:
       return {
         ...state,
