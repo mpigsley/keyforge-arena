b0ef0a02ac9adf871454f2db2b4e3c7603770983
Mitchel Pigsley <mpigsley@gmail.com>
2019-01-27 12:12:24 -0600

    Render piles for current player's side

--- src/components/game-board/card-piles/index.js
+++ src/components/game-board/card-piles/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import FlexContainer from 'primitives/flex-container';
 
 import { CardsType } from 'constants/types';
 import { useDimensions } from 'utils/effects';
 
 import styles from './styles.module.scss';
 
+const MAX_CARD_WIDTH = 140;
+const CARD_RATIO = 300 / 420;
 const HORIZONTAL_PADDING = 20;
-const VERTICAL_PADDING = 8;
+const STATIC_PILE_WIDTH = 100;
+const STATIC_PILE_HEIGHT = 75;
+const PADDING = 8;
 
 export default function CardPiles({
   className,
@@ -17,13 +24,67 @@ export default function CardPiles({
   archived,
 }) {
   const { height, width } = useDimensions();
-  const innerHeight = height * 0.3 - VERTICAL_PADDING * 2;
+  const innerHeight = height * 0.3 - PADDING * 2;
   const innerWidth = width * (2 / 7) - HORIZONTAL_PADDING * 2;
+  const horizontalCardWidth =
+    (innerWidth - PADDING * 2 - STATIC_PILE_WIDTH) / 2;
+  const verticalCardWidth =
+    (innerHeight - PADDING - STATIC_PILE_HEIGHT) * CARD_RATIO;
+  const isVertical = verticalCardWidth > horizontalCardWidth;
+  const cardWidth = Math.min(
+    Math.max(verticalCardWidth, horizontalCardWidth),
+    MAX_CARD_WIDTH,
+  );
+  const cardHeight = cardWidth / CARD_RATIO;
+
+  const staticWidth = isVertical ? cardWidth : STATIC_PILE_WIDTH;
+  const staticHeight = isVertical
+    ? STATIC_PILE_HEIGHT
+    : (cardHeight - PADDING) / 2;
+  const staticContainerStyle = isVertical
+    ? { marginBottom: PADDING }
+    : { marginLeft: PADDING };
+  const staticPiles = (
+    <FlexContainer
+      direction={isVertical ? 'row' : 'column'}
+      style={staticContainerStyle}
+    >
+      <div
+        className={styles.purgePile}
+        style={{
+          width: staticWidth,
+          height: staticHeight,
+          marginRight: isVertical ? PADDING : 0,
+          marginBottom: isVertical ? 0 : PADDING,
+        }}
+      />
+      <div
+        className={styles.archivePile}
+        style={{ width: staticWidth, height: staticHeight }}
+      />
+    </FlexContainer>
+  );
+
   return (
     <div
-      className={className}
-      style={{ padding: `${VERTICAL_PADDING}px ${HORIZONTAL_PADDING}px` }}
-    />
+      className={classNames(styles.container, className)}
+      style={{ padding: `${PADDING}px ${HORIZONTAL_PADDING}px` }}
+    >
+      {isVertical && staticPiles}
+      <FlexContainer
+        className={classNames({ [styles['row--horizontal']]: !isVertical })}
+      >
+        <div
+          className={styles.drawPile}
+          style={{ width: cardWidth, height: cardHeight, marginRight: PADDING }}
+        />
+        <div
+          className={styles.discardPile}
+          style={{ width: cardWidth, height: cardHeight }}
+        />
+        {!isVertical && staticPiles}
+      </FlexContainer>
+    </div>
   );
 }
 
