0d659280424bd9b65ab772c954e89a203e01eceb
Mitchel Pigsley <mpigsley@gmail.com>
2019-02-24 19:36:26 -0600

    Simplify and batch operations in create game

--- functions/src/create-game.js
+++ functions/src/create-game.js
@@ -49,34 +49,13 @@ module.exports = functions.https.onCall(async ({ lobby, deck }, context) => {
     if (!lobbyDoc.exists) {
       throw new functions.https.HttpsError('not-found', 'Lobby was not found.');
     }
+    const lobby = lobbyDoc.data();
 
-    const { players } = lobbyDoc.data();
-    const playerLobbies = await Promise.all(
-      players.map(uid =>
-        firestore
-          .collection('lobby')
-          .where('players', 'array-contains', uid)
-          .get(),
-      ),
-    );
-
-    const lobbyIds = uniq(
-      flatten(playerLobbies.map(snapshot => snapshot.docs.map(doc => doc.id))),
-    );
-    await Promise.all(
-      lobbyIds.map(id =>
-        firestore
-          .collection('lobby')
-          .doc(id)
-          .delete(),
-      ),
-    );
-
-    const opponent = players.filter(uid => uid !== context.auth.uid)[0];
+    const opponent = lobby.players.filter(uid => uid !== context.auth.uid)[0];
     const [opponentDeckSnapshot, playerDeckSnapshot] = await Promise.all([
       firestore
         .collection('decks')
-        .where('creator', '==', opponent)
+        .doc(lobby.deck)
         .get(),
       firestore
         .collection('decks')
@@ -87,32 +66,16 @@ module.exports = functions.https.onCall(async ({ lobby, deck }, context) => {
     let playerDeck;
     if (playerDeckSnapshot.exists) {
       playerDeck = playerDeckSnapshot.data();
-    } else {
+    }
+    let opponentDeck;
+    if (opponentDeckSnapshot.exists) {
+      opponentDeck = opponentDeckSnapshot.data();
+    }
+    if (!playerDeck || !opponentDeck) {
       throw new functions.https.HttpsError('not-found', 'Deck was not found.');
     }
 
-    let opponentDecks = {};
-    opponentDeckSnapshot.forEach(doc => {
-      opponentDecks = {
-        ...opponentDecks,
-        [doc.id]: doc.data(),
-      };
-    });
-
-    let opponentDeckId = findKey(opponentDecks, { selected: true });
-    if (!opponentDeckId) {
-      const { deckId } = sortBy(opponentDecks, 'name')[0];
-      opponentDeckId = findKey(opponentDecks, { deckId });
-    }
-
-    if (!opponentDeckId) {
-      throw new functions.https.HttpsError(
-        'not-found',
-        'Opponent deck was not found.',
-      );
-    }
-
-    const firstPlayer = chance.pickone(players);
+    const firstPlayer = chance.pickone(lobby.players);
     const playerHandSize = firstPlayer === context.auth.uid ? 7 : 6;
     const opponentHandSize = firstPlayer === opponent ? 7 : 6;
     const [playerHand, shuffledPlayerDeck] = shuffleAndDrawHand(
@@ -120,58 +83,83 @@ module.exports = functions.https.onCall(async ({ lobby, deck }, context) => {
       playerHandSize,
     );
     const [opponentHand, shuffledOpponentDeck] = shuffleAndDrawHand(
-      createDeck(opponentDecks[opponentDeckId]),
+      createDeck(opponentDeck),
       opponentHandSize,
     );
-    const gameRef = firestore.collection('games').doc();
 
-    await Promise.all([
+    const gameRef = firestore.collection('games').doc();
+    const batch = firestore.batch();
+    batch.set(
       firestore
         .collection('games')
         .doc(gameRef.id)
         .collection('personal')
-        .doc(context.auth.uid)
-        .set({ hand: playerHand, archived: [] }),
+        .doc(context.auth.uid),
+      { hand: playerHand, archived: [] },
+    );
+    batch.set(
       firestore
         .collection('games')
         .doc(gameRef.id)
         .collection('hidden')
-        .doc(context.auth.uid)
-        .set({ deck: shuffledPlayerDeck }),
+        .doc(context.auth.uid),
+      { deck: shuffledPlayerDeck },
+    );
+    batch.set(
       firestore
         .collection('games')
         .doc(gameRef.id)
         .collection('personal')
-        .doc(opponent)
-        .set({ hand: opponentHand, archived: [] }),
+        .doc(opponent),
+      { hand: opponentHand, archived: [] },
+    );
+    batch.set(
       firestore
         .collection('games')
         .doc(gameRef.id)
         .collection('hidden')
-        .doc(opponent)
-        .set({ deck: shuffledOpponentDeck }),
-      gameRef.set({
-        created: admin.firestore.FieldValue.serverTimestamp(),
-        isFinished: false,
-        turn: firstPlayer,
-        lobby,
-        state: {
-          [context.auth.uid]: {
-            ...initialSharedState(),
-            handSize: playerHandSize,
-            deckSize: 36 - playerHandSize,
-            deck,
-          },
-          [opponent]: {
-            ...initialSharedState(),
-            handSize: opponentHandSize,
-            deckSize: 36 - opponentHandSize,
-            deck: opponentDeckId,
-          },
+        .doc(opponent),
+      { deck: shuffledOpponentDeck },
+    );
+
+    // Cleanup old lobbies
+    const playerLobbies = await Promise.all(
+      lobby.players.map(uid =>
+        firestore
+          .collection('lobby')
+          .where('players', 'array-contains', uid)
+          .get(),
+      ),
+    );
+    const lobbyIds = uniq(
+      flatten(playerLobbies.map(snapshot => snapshot.docs.map(doc => doc.id))),
+    );
+    lobbyIds.map(id => batch.delete(firestore.collection('lobby').doc(id)));
+
+    await batch.commit();
+
+    // Create game last because client will try to get personal objects immediately afterword
+    await gameRef.set({
+      created: admin.firestore.FieldValue.serverTimestamp(),
+      isFinished: false,
+      turn: firstPlayer,
+      lobby,
+      state: {
+        [context.auth.uid]: {
+          ...initialSharedState(),
+          handSize: playerHandSize,
+          deckSize: 36 - playerHandSize,
+          deck,
         },
-        players,
-      }),
-    ]);
+        [opponent]: {
+          ...initialSharedState(),
+          handSize: opponentHandSize,
+          deckSize: 36 - opponentHandSize,
+          deck: lobby.deck,
+        },
+      },
+      players: lobby.players,
+    });
 
     return gameRef.id;
   } catch (e) {
