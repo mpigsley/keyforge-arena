48aeafd5e7e1e3a0f8d0fb4882161531c41ffda5
Mitchel Pigsley <mpigsley@gmail.com>
2019-03-03 08:29:33 -0600

    Add single drop view in empty battleline

--- src/components/game-board/battleline/index.js
+++ src/components/game-board/battleline/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import FlexContainer from 'primitives/flex-container';
@@ -18,46 +18,68 @@ import styles from './styles.module.scss';
 const CARD_PADDING = 10;
 
 export default function Battleline({ cards, isOpponent }) {
+  const [isAcceptingCard, setIsAcceptingCard] = useState(false);
+  const cardNum = cards.length + (isAcceptingCard ? 1 : 0);
   const { height, width } = useDimensions();
   const battlelineHeight = height / 4 - VERTICAL_PADDING * 2;
-  const paddingWidth = (cards.length - 1) * CARD_PADDING;
+  const paddingWidth = (cardNum - 1) * CARD_PADDING;
   const innerWidth = width - HORIZONTAL_PADDING * 2 - paddingWidth;
 
-  const defaultWidth = innerWidth / cards.length;
+  const defaultWidth = innerWidth / cardNum;
   const scaledWidth = Math.min(
     battlelineHeight * CARD_RATIO,
     defaultWidth,
     150,
   );
   const zoomScale = ZOOMED_WIDTH / scaledWidth;
-  const cardsWidth = cards.length * scaledWidth + paddingWidth;
+  const cardsWidth = cardNum * scaledWidth + paddingWidth;
   const scaleOverhang = (ZOOMED_WIDTH - scaledWidth) / 2;
   const extraSideWidth = (width - cardsWidth) / 2;
   const needTranslate = scaleOverhang > extraSideWidth;
 
-  return (
-    <FlexContainer
-      direction="column"
-      onDragOver={e => e.preventDefault()}
-      onDrop={e => {
+  let dragEvents = {};
+  if (!isOpponent) {
+    dragEvents = {
+      onDragEnter: () => setIsAcceptingCard(true),
+      onDragLeave: () => setIsAcceptingCard(false),
+      onDragOver: e => e.preventDefault(),
+      onDrop: e => {
         e.preventDefault();
         const card = e.dataTransfer.getData('card');
+        setIsAcceptingCard(false);
         console.log(card); // eslint-disable-line
-      }}
+      },
+    };
+  }
+
+  return (
+    <FlexContainer
+      {...dragEvents}
+      direction="column"
       className={styles.battleline}
       justify={isOpponent ? 'flexEnd' : 'flexStart'}
       style={{ padding: `${VERTICAL_PADDING}px ${HORIZONTAL_PADDING}px` }}
     >
-      <FlexContainer justify="center">
+      <FlexContainer justify="center" className={styles.inner}>
+        {isAcceptingCard && (
+          <div
+            className={styles.cardOutline}
+            style={{
+              width: `${scaledWidth}px`,
+              height: `${scaledWidth / CARD_RATIO}px`,
+              paddingRight: cardNum > 1 ? `${CARD_PADDING / 2}px` : 0,
+            }}
+          />
+        )}
         {cards.map(({ expansion, card }, i) => (
           <div
             key={`${card}-${i}` /* eslint-disable-line */}
             className={styles.imgContainer}
             style={{
               width: `${scaledWidth}px`,
-              paddingLeft: i !== 0 ? `${CARD_PADDING / 2}px` : 0,
-              paddingRight:
-                i !== cards.length - 1 ? `${CARD_PADDING / 2}px` : 0,
+              paddingLeft:
+                i !== 0 || isAcceptingCard ? `${CARD_PADDING / 2}px` : 0,
+              paddingRight: i !== cardNum - 1 ? `${CARD_PADDING / 2}px` : 0,
             }}
           >
             <Card
@@ -68,7 +90,7 @@ export default function Battleline({ cards, isOpponent }) {
                 const offset = (ZOOMED_WIDTH - scaledWidth) / (zoomScale * 2);
                 if (i === 0 && needTranslate) {
                   e.target.style.transform = `scale(${zoomScale}) translate(${offset}px)`;
-                } else if (i === cards.length - 1 && needTranslate) {
+                } else if (i === cardNum - 1 && needTranslate) {
                   e.target.style.transform = `scale(${zoomScale}) translate(-${offset}px)`;
                 } else {
                   e.target.style.transform = `scale(${zoomScale})`;
